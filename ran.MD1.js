4792c5eb9815f1e5003d7ba0e9c4ab3357c6e245
unknown <bazo20061996@gmail.com>
2022-07-15 15:28:07 +0700

    Game ran cap nhat lan 3

--- ran.MD1.js
+++ ran.MD1.js
@@ -1,12 +1,14 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+
 //tạo lớp để làm fun kiểm tra độ dài của rắn
 class SnakeChieuDai {
-    constructor(x,y) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 }
+
 let speed = 7;
 let buocDi = 20;
 let dauRanX = 10;
@@ -14,81 +16,86 @@ let dauranY = 10;
 let tileSize = canvas.width / buocDi - 2;
 let thanRan = [];
 let doDaiThan = 0;
+
 /*biến vận tốc dùng để ghép với  fun di chuyển keycode*/
 let speedX = 0;
 let speedY = 0;
-//biến mồi
+
+//biến mồi////////////////////////////////////////////////////////
 let moiX = 5;
 let moiY = 5;
-//Biến tính điểm
+
+//Biến tính điểm//////////////////////////////////////////////////
 let score = 0;
 let amthanh = new Audio("audio/oigoioi.mp3")
-let amThanh2 = new  Audio("audio/Xin là xin vĩnh biệt cụ.mp3")
+let amThanh2 = new Audio("audio/Xin là xin vĩnh biệt cụ.mp3")
 
-
-//fun bat dau game
+// fun bat dau game///////////////////////////////////////////////
 function starGame() {
     dauRan();
-let ketQua = gameOver();
-if(ketQua){
-    return;
-}
+    let ketQua = gameOver();
+    if (ketQua) {
+        return;
+    }
     xoaManHinh();
     checkVaCham();
     veMoi();
     veSnake();
     tinhDiem();
-    if (score >3){
+    if (score > 3) {
         speed = 10;
     }
-    if (score >6){
+    if (score > 6) {
         speed = 15
     }
     setTimeout(starGame, 1000 / speed)
-}starGame();
+}
+
+starGame();
+
 /////fun ket thuc game
 function gameOver() {
     let ketThuc = false;
+
     //nếu vận tốc của x y bằng 0 tức là chưa bắt đầu game
-    if (speedX ===0 && speedY ===0){
+    if (speedX === 0 && speedY === 0) {
         return false;
     }
     ///kiểm tra các điểm va chạm bên tường
-    if (dauRanX <0){
+    if (dauRanX < 0) {
         ketThuc = true;
-    }
-    else if (dauRanX >= buocDi){
+    } else if (dauRanX >= buocDi) {
         ketThuc = true;
-    }
-    else if (dauranY < 0){
-        ketThuc =true
-    }
-    else if (dauranY >= buocDi){
+    } else if (dauranY < 0) {
+        ketThuc = true
+    } else if (dauranY >= buocDi) {
         ketThuc = true;
     }
     ////kiểm tra cắt đuôi để kết thúc
     for (let i = 0; i < thanRan.length; i++) {
         let diemCat = thanRan[i];
-        if (diemCat.x===dauRanX && diemCat.y === dauranY){
+        if (diemCat.x === dauRanX && diemCat.y === dauranY) {
             ketThuc = true;
             break;
         }
     }
     ////////Nếu kết thúc thì retun chữ game over
-    if (ketThuc){
+    if (ketThuc) {
         amThanh2.play();
         ctx.fillStyle = "white";
         ctx.font = "50px Verdana";
-        ctx.fillText("Game Over! ",canvas.width/8,canvas.height/2);
+        ctx.fillText("Game Over! ", canvas.width / 8, canvas.height / 2);
 
     }
     return ketThuc;
 }
+
 function tinhDiem() {
     ctx.fillStyle = "white";
     ctx.font = "10px Verdana";
-    ctx.fillText("Score " + score,canvas.width - 50,10);
+    ctx.fillText("Score " + score, canvas.width - 50, 10);
 }
+
 ////////////////////////////////////////////////////////////////////////////
 function xoaManHinh() {
     ctx.fillStyle = "black";
@@ -99,17 +106,21 @@ function xoaManHinh() {
 function veSnake() {
     ctx.fillStyle = "orange";
     ctx.fillRect(dauRanX * buocDi, dauranY * buocDi, tileSize, tileSize);
-    //lệnh vẽ thân rắn sau khki ăn mồi
+
+    //lệnh vẽ thân rắn sau khi ăn mồi
     ctx.fillStyle = "green";
     for (let i = 0; i < thanRan.length; i++) {
-      let ve = thanRan[i];
-            ctx.fillRect(ve.x * buocDi,ve.y * buocDi,tileSize,tileSize)
+        let ve = thanRan[i];
+        ctx.fillRect(ve.x * buocDi, ve.y * buocDi, tileSize, tileSize)
     }
+
     //vẽ thêm thân rắn và nếu thân rắn dài hơn độ dài thân cũ  thì sẽ xóa cái cũ đi và hiểm thị cái mới nhất
-    thanRan.push(new SnakeChieuDai(dauRanX,dauranY));
-    if (thanRan.length>doDaiThan){
+    thanRan.push(new SnakeChieuDai(dauRanX, dauranY));
+    if (thanRan.length > doDaiThan) {
         thanRan.shift();
     }
+    ctx.fillStyle = "orange";
+    ctx.fillRect(dauRanX * buocDi, dauranY * buocDi, tileSize, tileSize);
 }
 
 // tạo fun về đầu rắn
@@ -123,50 +134,99 @@ function veMoi() {
     ctx.fillStyle = "red";
     ctx.fillRect(moiX * buocDi, moiY * buocDi, tileSize, tileSize)
 }
+
 ////// tạo fun check va chạm mồi/////////////////////////////
 function checkVaCham() {
-    if (moiX===dauRanX&&moiY===dauranY){
-        moiX =Math.floor(Math.random()*buocDi);
-        moiY =Math.floor(Math.random()*buocDi);
-        doDaiThan ++ ;
+    if (moiX === dauRanX && moiY === dauranY) {
+        moiX = Math.floor(Math.random() * buocDi);
+        moiY = Math.floor(Math.random() * buocDi);
+        doDaiThan++;
         score++;
         amthanh.play();
     }
 }
 
+const delay = debounce((e) => keyDown(e),100)
+
 //tao fun di chuyen sẽ kết hợp với biến vận tốc //
-document.body.addEventListener("keydown", keyDown);
+document.body.addEventListener("keydown", delay);
 
 function keyDown(event) {
-// di chuyển lên
-    if (event.keyCode == 38) {
-        if (speedY == 1)
-            return;
-        speedY = -1;
-        speedX = 0;
-    }
-    // di chuyển xuong
-    if (event.keyCode == 40) {
-        if (speedY == -1)
-            return;
-        speedY = 1;
-        speedX = 0;
-    }
-    // di chuyển trai
-    if (event.keyCode == 37) {
-        if (speedX == 1)
-            return;
-        speedY = 0;
-        speedX = -1;
-    }
-    // di chuyển phai
-    if (event.keyCode == 39) {
-        if (speedX == -1)
-            return;
-        speedY = 0;
-        speedX = 1;
+    switch (event.keyCode) {
+        case 38:
+            if (speedY === 1)
+                return;
+            speedY = -1;
+            speedX = 0;
+            break;
+        case 40:
+            if (speedY === -1)
+                return;
+            speedY = 1;
+            speedX = 0;
+            break;
+        case 37:
+            if (speedX === 1)
+                return;
+            speedY = 0;
+            speedX = -1;
+            break;
+        case 39:
+            if (speedX === -1)
+                return;
+            speedY = 0;
+            speedX = 1;
+            break;
     }
 }
+
 function loadLai() {
     window.location.reload();
 }
+
+
+function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+        if (!timer) {
+            func.apply(this, args);
+        }
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            timer = undefined;
+        }, timeout);
+    };
+}
+
+
+// function keyDown(event) {
+// // di chuyển lên
+//     if (event.keyCode == 38) {
+//         if (speedY == 1)
+//             return;
+//         speedY = -1;
+//         speedX = 0;
+//     }
+//     // di chuyển xuong
+//     if (event.keyCode == 40) {
+//         if (speedY == -1)
+//             return;
+//         speedY = 1;
+//         speedX = 0;
+//     }
+//     // di chuyển trai
+//     if (event.keyCode == 37) {
+//         if (speedX == 1)
+//             return;
+//         speedY = 0;
+//         speedX = -1;
+//     }
+//     // di chuyển phai
+//     if (event.keyCode == 39) {
+//         if (speedX == -1)
+//             return;
+//         speedY = 0;
+//         speedX = 1;
+//     }
+// }
+
